1c58e17f95b77f149eea50ae145bce0855f9462c
Doguhan Uluca <duluca@gmail.com>
2019-12-20 19:57:23 -0500

    applied new settings

--- src/index.ts
+++ src/index.ts
@@ -1,272 +1,245 @@
+import { install, json, lines, makeDirs, packageJson } from 'mrm-core'
+
+import { BaseVsCodeSettings } from '../shared'
+import { BaseVsCodeExtensions } from '../shared/baseVsCodeExtensions'
 import {
   configureCommonNpmPackages,
   configureImportSort,
   configureInitEnv,
   configurePRTemplate,
   configurePrettier,
-  configureTsLint
-} from "../shared/commonTasks";
-import { install, json, lines, makeDirs, packageJson } from "mrm-core";
-
-import { BaseVsCodeSettings } from "../shared";
-import { setScripts } from "../shared/helpers";
+  configureTsLint,
+} from '../shared/commonTasks'
+import { setScripts } from '../shared/helpers'
 
 function task() {
-  configureCommonNpmPackages();
-  configureNpmScripts();
-  configureImportSort();
-  configureTypeScript();
-  configureBaseTsLint();
-  configurePrettier();
-  configureVsCodeForTypeScript();
-  configureJasmineAndNyc();
-  configureInitEnv();
-  configurePRTemplate();
+  configureCommonNpmPackages()
+  configureNpmScripts()
+  configureImportSort()
+  configureTypeScript()
+  configureBaseTsLint()
+  configurePrettier()
+  configureVsCodeForTypeScript()
+  configureJasmineAndNyc()
+  configureInitEnv()
+  configurePRTemplate()
 }
-
 
 function configureTypeScript() {
   install(['typescript', '@types/node'])
 
-  json('tsconfig.json').merge(
-    {
-      "compilerOptions": {
-        "target": "es2015",
-        "module": "commonjs",
-        "moduleResolution": "node",
-        "lib": [
-          "es2015",
-          "es2016",
-          "es2017",
-          "es2018",
-          "es2019"
-        ],
-        "typeRoots": [
-          "node_modules/@types"
-        ],
-        "resolveJsonModule": true
-      }
-    }
-  )
+  json('tsconfig.json').merge({
+    compilerOptions: {
+      target: 'es2015',
+      module: 'commonjs',
+      moduleResolution: 'node',
+      lib: ['es2015', 'es2016', 'es2017', 'es2018', 'es2019'],
+      typeRoots: ['node_modules/@types'],
+      resolveJsonModule: true,
+    },
+  })
 
-  json('tsconfig.src.json').merge(
-    {
-      "extends": "./tsconfig.json",
-      "compilerOptions": {
-    
-        "outDir": "./dist",
-        "strict": true,
-        "noImplicitAny": true,
-        "strictNullChecks": true,
-        "noImplicitThis": true,
-        "alwaysStrict": true,
-    
-        "noUnusedLocals": true,
-        "noUnusedParameters": true,
-        "noImplicitReturns": true,
-        "noFallthroughCasesInSwitch": true,
-        "experimentalDecorators": true,
-        "strictPropertyInitialization": false,
-    
-        "sourceMap": true
-    
-      },
-      "include": [
-        "./src"
-      ]
-    }
-  )
+  json('tsconfig.src.json').merge({
+    extends: './tsconfig.json',
+    compilerOptions: {
+      outDir: './dist',
+      strict: true,
+      noImplicitAny: true,
+      strictNullChecks: true,
+      noImplicitThis: true,
+      alwaysStrict: true,
+
+      noUnusedLocals: true,
+      noUnusedParameters: true,
+      noImplicitReturns: true,
+      noFallthroughCasesInSwitch: true,
+      experimentalDecorators: true,
+      strictPropertyInitialization: false,
+
+      sourceMap: true,
+    },
+    include: ['./src'],
+  })
 
   makeDirs(['src', 'dist'])
 
-  if(!lines('src/index.ts').exists) {
+  if (!lines('src/index.ts').exists) {
     lines('src/index.ts', [
-      'export const message = \'Hello, world!\'',
+      "export const message = 'Hello, world!'",
       'console.log(message)',
-      '// Execute \'npm run build\' to build your code',
-      '// Execute \'npm start\' to build and run your code',
-      '// Execute \'npm test\' to run your tests'
+      "// Execute 'npm run build' to build your code",
+      "// Execute 'npm start' to build and run your code",
+      "// Execute 'npm test' to run your tests",
     ])
   }
 }
 
 function configureJasmineAndNyc() {
-  const jasminePackages = ['ts-node', 'jasmine', 'jasmine-node', 'jasmine-xml-reporter', '@types/jasmine']
+  const jasminePackages = [
+    'ts-node',
+    'jasmine',
+    'jasmine-node',
+    'jasmine-xml-reporter',
+    '@types/jasmine',
+  ]
   const nycPackages = ['@istanbuljs/nyc-config-typescript', 'nyc', 'source-map-support']
   install(jasminePackages.concat(nycPackages))
 
-  const pkg = packageJson();
+  const pkg = packageJson()
 
   setScripts(pkg, {
-    "pretest": "npm run build && npm run build:test",
-    "test": "ts-node node_modules/jasmine/bin/jasmine --config=./jasmine.json",
-    "test:ci": "ts-node ./node_modules/jasmine-xml-reporter/bin/jasmine.js --config=./jasmine.json --junitreport --output=test_results/",
-    "test:nyc": "nyc ts-node node_modules/jasmine/bin/jasmine --config=./jasmine.json --cache=false"
+    pretest: 'npm run build && npm run build:test',
+    test: 'ts-node node_modules/jasmine/bin/jasmine --config=./jasmine.json',
+    'test:ci':
+      'ts-node ./node_modules/jasmine-xml-reporter/bin/jasmine.js --config=./jasmine.json --junitreport --output=test_results/',
+    'test:nyc':
+      'nyc ts-node node_modules/jasmine/bin/jasmine --config=./jasmine.json --cache=false',
   })
 
   json('jasmine.json').merge({
-    "spec_dir": "tests",
-    "spec_files": [
-      "**/*[sS]pec.ts"
-    ],
-    "stopSpecOnExpectationFailure": false,
-    "random": true
+    spec_dir: 'tests',
+    spec_files: ['**/*[sS]pec.ts'],
+    stopSpecOnExpectationFailure: false,
+    random: true,
   })
-  
+
   json('.nycrc').merge({
-    "extends": "@istanbuljs/nyc-config-typescript",
-    "all": true,
-    "check-coverage": true,
-    "extension": [
-      ".ts",
-      ".tsx"
-    ],
-    "exclude": [
-      "**/*.d.ts",
-      "**/tests",
-      "**/coverage"
-    ],
-    "reporter": [
-      "lcov",
-      "text"
-    ],
-    "branches": 90,
-    "lines": 90,
-    "functions": 90,
-    "statements": 90
+    extends: '@istanbuljs/nyc-config-typescript',
+    all: true,
+    'check-coverage': true,
+    extension: ['.ts', '.tsx'],
+    exclude: ['**/*.d.ts', '**/tests', '**/coverage'],
+    reporter: ['lcov', 'text'],
+    branches: 90,
+    lines: 90,
+    functions: 90,
+    statements: 90,
   })
 
   makeDirs('tests')
-  json('tests/tsconfig.spec.json').merge(
-    {
-      "extends": "../tsconfig.json",
-      "compilerOptions": {
-        "declaration": false,
-        "strict": false,
-        "sourceMap": true,
-        "noImplicitAny": false,
-        "types": [
-          "node",
-          "jasmine"
-        ],
-      }
-    }
-  )
+  json('tests/tsconfig.spec.json').merge({
+    extends: '../tsconfig.json',
+    compilerOptions: {
+      declaration: false,
+      strict: false,
+      sourceMap: true,
+      noImplicitAny: false,
+      types: ['node', 'jasmine'],
+    },
+  })
 
-  if(!lines('tests/index.spec.ts').exists) {
+  if (!lines('tests/index.spec.ts').exists) {
     lines('tests/index.spec.ts', [
-      'import { message } from \'../src/index\'',
+      "import { message } from '../src/index'",
       '',
-      'describe(\'Index\', () => {',
-      '  it(\'should have the correct message\', () => {',
-      '    expect(\'Hello, world!\').toEqual(message)',
+      "describe('Index', () => {",
+      "  it('should have the correct message', () => {",
+      "    expect('Hello, world!').toEqual(message)",
       '  })',
-      '})'
+      '})',
     ])
   }
 }
 
 function configureNpmScripts() {
   install('env-cmd')
-  const pkg = packageJson();
+  const pkg = packageJson()
 
   setScripts(pkg, {
     style:
       'import-sort -l "{lib,tests}/**/*.ts" && prettier --check "{lib,tests}/**/*.ts"',
-    "style:fix":
+    'style:fix':
       'import-sort --write "{lib,tests}/**/*.ts" && prettier --write "{lib,tests}/**/*.ts"',
-    lint: "tslint --config tslint.json --project .",
-    "lint:fix": "tslint --config tslint.json --fix --project .",
-    "prestart": "npm run build",
-    "start": "env-cmd node dist/index.js",
-    "prebuild": "rimraf dist",
-    "build": "tsc -p tsconfig.src.json",
-    "build:test": "tsc -p tests/tsconfig.spec.json",
-    "prepublishOnly": "npm run test",
-    "prepare": "npm run build"
-  });
+    lint: 'tslint --config tslint.json --project .',
+    'lint:fix': 'tslint --config tslint.json --fix --project .',
+    prestart: 'npm run build',
+    start: 'env-cmd node dist/index.js',
+    prebuild: 'rimraf dist',
+    build: 'tsc -p tsconfig.src.json',
+    'build:test': 'tsc -p tests/tsconfig.spec.json',
+    prepublishOnly: 'npm run test',
+    prepare: 'npm run build',
+  })
 }
 
 function configureBaseTsLint() {
-  json('tslint.json').merge(
-    {
-      "rules": {
-        "array-type": false,
-        "arrow-parens": false,
-        "deprecation": {
-          "severity": "warn"
+  json('tslint.json').merge({
+    rules: {
+      'array-type': false,
+      'arrow-parens': false,
+      deprecation: {
+        severity: 'warn',
+      },
+      'import-blacklist': [true, 'rxjs/Rx'],
+      'interface-name': false,
+      'max-classes-per-file': false,
+      'member-access': false,
+      'member-ordering': [
+        true,
+        {
+          order: ['static-field', 'instance-field', 'static-method', 'instance-method'],
         },
-        "import-blacklist": [true, "rxjs/Rx"],
-        "interface-name": false,
-        "max-classes-per-file": false,
-        "member-access": false,
-        "member-ordering": [
-          true,
-          {
-            "order": ["static-field", "instance-field", "static-method", "instance-method"]
-          }
-        ],
-        "no-consecutive-blank-lines": false,
-        "no-console": [true, "debug", "info", "time", "timeEnd", "trace"],
-        "no-empty": false,
-        "no-inferrable-types": [true, "ignore-params"],
-        "no-non-null-assertion": true,
-        "no-redundant-jsdoc": true,
-        "no-switch-case-fall-through": true,
-        "no-var-requires": false,
-        "object-literal-key-quotes": [true, "as-needed"],
-        "object-literal-sort-keys": false,
-        "ordered-imports": false,
-        "trailing-comma": false,
-      }
-    }
-  )
-  configureTsLint();
+      ],
+      'no-consecutive-blank-lines': false,
+      'no-console': [true, 'debug', 'info', 'time', 'timeEnd', 'trace'],
+      'no-empty': false,
+      'no-inferrable-types': [true, 'ignore-params'],
+      'no-non-null-assertion': true,
+      'no-redundant-jsdoc': true,
+      'no-switch-case-fall-through': true,
+      'no-var-requires': false,
+      'object-literal-key-quotes': [true, 'as-needed'],
+      'object-literal-sort-keys': false,
+      'ordered-imports': false,
+      'trailing-comma': false,
+    },
+  })
+  configureTsLint()
 }
 
 function configureVsCodeForTypeScript() {
-  json(".vscode/extensions.json")
+  json('.vscode/extensions.json')
     .merge({
-      recommendations: BaseVsCodeSettings
+      recommendations: BaseVsCodeExtensions,
     })
-    .save();
+    .save()
 
-  json(".vscode/settings.json")
+  json('.vscode/settings.json')
     .merge(
       Object.assign(BaseVsCodeSettings, {
-        "files.exclude": {
-          "**/.git": true,
-          "**/.DS_Store": true,
-          "**/*.js": {
-            when: "$(basename).ts"
+        'files.exclude': {
+          '**/.git': true,
+          '**/.DS_Store': true,
+          '**/*.js': {
+            when: '$(basename).ts',
           },
-          "**/*.js.map": {
-            when: "$(basename)"
-          }
-        }
+          '**/*.js.map': {
+            when: '$(basename)',
+          },
+        },
       })
     )
-    .save();
+    .save()
 
-  json(".vscode/launch.json")
+  json('.vscode/launch.json')
     .merge({
-      version: "0.2.0",
+      version: '0.2.0',
       configurations: [
         {
-          name: "Jasmine Current Spec File",
-          type: "node",
-          request: "launch",
-          program: "${workspaceRoot}/node_modules/jasmine/bin/jasmine.js",
-          args: ["tests/${fileBasenameNoExtension}.js"],
+          name: 'Jasmine Current Spec File',
+          type: 'node',
+          request: 'launch',
+          program: '${workspaceRoot}/node_modules/jasmine/bin/jasmine.js',
+          args: ['tests/${fileBasenameNoExtension}.js'],
           env: {
-            NODE_PATH: "."
+            NODE_PATH: '.',
           },
-          preLaunchTask: "npm: build:test"
-        }
-      ]
+          preLaunchTask: 'npm: build:test',
+        },
+      ],
     })
-    .save();
+    .save()
 }
 
-task.description = "Configures VS Code for TypeScript projects";
+task.description = 'Configures VS Code for TypeScript projects'
 module.exports = task
